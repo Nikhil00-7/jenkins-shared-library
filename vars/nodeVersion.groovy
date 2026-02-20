3ff1764314c33462169b24bbe89da037ed423293
Nikhil <kujurnikhil0007@gmail.com>
2026-02-19 23:44:05 -0800

    chek

---
+++ vars/nodeVersion.groovy
@@ -0,0 +1,18 @@
+def call(String  nodeVersion= "node22"){
+    echo "using node version is ${nodeVersion}"
+
+    pipeline {
+
+        angent any 
+
+        tools {
+            node "${nodeVersion}"
+        }
+
+        stages{
+            stage("Version"){
+             echo "This pipeline is running on Version ${nodeVersion}"
+            }
+        }
+    }
+}
