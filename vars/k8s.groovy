c8a2e95f8a418b5a7a30e9bdd1f4039eef99eb67
Nikhil <kujurnikhil0007@gmail.com>
2026-02-20 00:43:46 -0800

    mismatch

--- vars/k8s.groovy
+++ vars/k8s.groovy
@@ -1,14 +1,14 @@
-def call (String k8s-cred= "k8s-master-ssh" , String k8s-ip= "192.168.2.20" ){
+def call (String k8s_cred= "k8s-master-ssh" , String k8s_ip= "192.168.2.20" ){
     
     echo "copying files in k8s-VM"
     
-    sshagent(["${k8s-cred}"]){
+    sshagent(["${k8s_cred}"]){
         sh ''' 
-        scp -o StrictHostKeyChecking=no jenkinsOne.yaml ubuntu@${k8sIp}:/home/ubuntu/
-            scp -o StrictHostKeyChecking=no service.yaml ubuntu@${k8sIp}:/home/ubuntu/
+        scp -o StrictHostKeyChecking=no jenkinsOne.yaml ubuntu@${k8s_ip}:/home/ubuntu/
+            scp -o StrictHostKeyChecking=no service.yaml ubuntu@${k8s_ip}:/home/ubuntu/
 
-            ssh -o StrictHostKeyChecking=no ubuntu@${k8sIp} "kubectl apply -f /home/ubuntu/jenkinsOne.yaml"
-            ssh -o StrictHostKeyChecking=no ubuntu@${k8sIp} "kubectl apply -f /home/ubuntu/service.yaml"
+            ssh -o StrictHostKeyChecking=no ubuntu@${k8s_ip} "kubectl apply -f /home/ubuntu/jenkinsOne.yaml"
+            ssh -o StrictHostKeyChecking=no ubuntu@${k8s_ip} "kubectl apply -f /home/ubuntu/service.yaml"
         '''
     }
 }
